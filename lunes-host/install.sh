58a456e0a0a377a228d34294d570aee017700778
crarm <61692711+crarm@users.noreply.github.com>
2026-01-07 10:50:39 +0800

    Update install.sh

--- lunes-host/install.sh
+++ lunes-host/install.sh
@@ -2,11 +2,12 @@
 
 DOMAIN="${DOMAIN:-node68.lunes.host}"
 PORT="${PORT:-10008}"
+PATH="${PATH:-/vmess}"
 UUID="${UUID:-2584b733-9095-4bec-a7d5-62b473540f7a}"
 HY2_PASSWORD="${HY2_PASSWORD:-vevc.HY2.Password}"
 
-curl -sSL -o app.js https://raw.githubusercontent.com/vevc/one-node/refs/heads/main/lunes-host/app.js
-curl -sSL -o package.json https://raw.githubusercontent.com/vevc/one-node/refs/heads/main/lunes-host/package.json
+curl -sSL -o app.js https://raw.githubusercontent.com/crarm/one-node/refs/heads/main/lunes-host/app.js
+curl -sSL -o package.json https://raw.githubusercontent.com/crarm/one-node/refs/heads/main/lunes-host/package.json
 
 mkdir -p /home/container/xy
 cd /home/container/xy
@@ -14,17 +15,13 @@ curl -sSL -o Xray-linux-64.zip https://github.com/XTLS/Xray-core/releases/downlo
 unzip Xray-linux-64.zip
 rm Xray-linux-64.zip
 mv xray xy
-curl -sSL -o config.json https://raw.githubusercontent.com/vevc/one-node/refs/heads/main/lunes-host/xray-config.json
+curl -sSL -o config.json https://raw.githubusercontent.com/crarm/one-node/refs/heads/main/lunes-host/xray-config.json
 sed -i "s/10008/$PORT/g" config.json
 sed -i "s/YOUR_UUID/$UUID/g" config.json
-keyPair=$(./xy x25519)
-privateKey=$(echo "$keyPair" | grep "Private key" | awk '{print $3}')
-publicKey=$(echo "$keyPair" | grep "Public key" | awk '{print $3}')
-sed -i "s/YOUR_PRIVATE_KEY/$privateKey/g" config.json
-shortId=$(openssl rand -hex 4)
-sed -i "s/YOUR_SHORT_ID/$shortId/g" config.json
-vlessUrl="vless://$UUID@$DOMAIN:$PORT?encryption=none&flow=xtls-rprx-vision&security=reality&sni=www.cloudflare.com&fp=chrome&pbk=$publicKey&sid=$shortId&spx=%2F&type=tcp&headerType=none#lunes-reality"
-echo $vlessUrl > /home/container/node.txt
+vmess="{\"v\": \"2\",\"ps\": \"lunes-vmess\",\"add\": $DOMAIN,\"port\": $PORT,\"id\": $UUID,\"aid\": \"64\",\"scy\": \"auto\",\"net\": \"ws\",\"type\": \"none\",\"host\": $DOMAIN,\"path\": $PATH,\"tls\": \"\",\"sni\": $DOMAIN,\"alpn\": \"\"}"
+
+vmessUrl="vless://"$(base64 -w 0 $vmess)
+echo $vmessUrl > /home/container/node.txt
 
 mkdir -p /home/container/h2
 cd /home/container/h2
@@ -41,6 +38,6 @@ echo $hy2Url >> /home/container/node.txt
 echo "============================================================"
 echo "🚀 VLESS Reality & HY2 Node Info"
 echo "------------------------------------------------------------"
-echo "$vlessUrl"
+echo "$vmessUrl"
 echo "$hy2Url"
 echo "============================================================"
